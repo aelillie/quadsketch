2068daf86c39f46d9d4a92441ab57a8bb5a35705
aelillie <anders@edelbolillie.dk>
2018-05-02 11:49:26 +0200

    Test results

--- baseline/test.sh
+++ baseline/test.sh
@@ -18,7 +18,7 @@ OUTFILE=$5
 for (( l=$START; l<=$END; l*=$STEP ))
 do
     echo "Running grid on $DATASET with $l landmarks to $OUTFILE"
-    ./grid -i ../datasets/$DATASET -o $OUTFILE -l $l -d >/dev/null
+    ./grid -i ../datasets/$DATASET -o $OUTFILE -l $l >/dev/null
 done
 
 echo "All done"

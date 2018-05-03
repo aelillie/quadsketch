5d18dc5bd7b92ff97b56491b86ffab9b19f6f5ad
aelillie <anders@edelbolillie.dk>
2018-05-03 20:44:33 +0200

    Complete test setup for a single dataset

--- src/test_qsr.sh
+++ src/test_qsr.sh
@@ -2,26 +2,25 @@
 if [ $# -lt 2 ]
     then
         echo "Not enough arguments supplied. Run with:"
-        echo "test_qsr.sh <start> <end> <step> <#blocks> <dataset> <outfile>"
+        echo "<dataset> <outfile>"
         exit 1
 fi
 
 make qsr
 
-START=$1
-END=$2
-STEP=$3
-BLOCKS=$4
-DATASET=$5
-OUTFILE=$6
+DATASET=$1
+OUTFILE=$2
 
-for (( k=$START; k<=$END; k++ ))
+for (( b=2; b<=32; b*=2 ))
 do
-    LAMBDA=$k
-    DEPTH=$((k+$STEP))
-    echo "Running qsr on $DATASET with blocks $BLOCKS, depth $DEPTH, and lambda $LAMBDA to $outfile"
-    ./qsr -i ../datasets/$DATASET -o $OUTFILE -n $BLOCKS -d $DEPTH -l $LAMBDA >/dev/null
+    for (( d=2; d<=20; d++ ))
+    do
+        for (( l=1; l<$d; l++ ))
+        do
+            echo "Running qsr on $DATASET with blocks $b, depth $d, and lambda $l to $OUTFILE"
+            ./qsr -i ../datasets/$DATASET -o $OUTFILE -n $b -d $d -l $l >/dev/null
+        done
+    done
 done
 
 echo "All done"
-#For example: qs -i ../datasets/sift -o sift_results.txt -n 8 -d 7 -l 5
